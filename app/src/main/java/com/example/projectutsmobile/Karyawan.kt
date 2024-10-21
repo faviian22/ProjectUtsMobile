eb6204c5840b4cddeb06afbb5376d49a486b4a95
LAPTOP-UPG5JS3M\User <anindyaputri231204@gmail.com>
2024-10-21 14:28:53 +0700

    Tambah Karyawan

---
+++ app/src/main/java/com/example/projectutsmobile/Karyawan.kt
@@ -0,0 +1,13 @@
+package com.example.projectutsmobile
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "karyawan_table")
+data class Karyawan(
+    @PrimaryKey(autoGenerate = true)
+    val id_karyawan: Int = 0,
+    val nama_karyawan: String,
+    val jenis_kelamin: String,
+    val alamat_karyawan: String
+)
